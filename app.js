76a5d4d957dfc88a340226c9fefc6537298327f1
khushboo <palkhushboo2632@gmail.com>
2024-08-08 11:05:16 +0530

    setup of ejs

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app=express();
 const http=require("http");
 const socketio=require("socket.io");
 const server=http.createServer(app);
-
+const path = require("path");
 
 const io=socketio(server);
 
-
+app.set("view engine","ejs");
+app.set(express.static(path.join(__dirname,"public")));
 app.get("/",(req,res)=>{
  res.send("hey");
 })
